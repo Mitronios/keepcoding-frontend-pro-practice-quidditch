f1c8b0a4e664a4c13d6817da8a528fffbc33a8a0
Miguel Angel Gonzales Melgar <mitronios@gmail.com>
2025-05-10 09:39:37 -0400

    Feature: add functional timer to show next edition.

--- src/lib/home.ts
+++ src/lib/home.ts
@@ -1,21 +1,42 @@
 import { DateTime } from "luxon";
 
-//Define next date
-
-// Define actual date
-
-// Calculate differences in days, hours, minutes, seconds
-
 // Update html values
+function showtimeElement(elementId: string, data: string): void {
+	const element = document.getElementById(elementId);
+	if (!element) {
+		return;
+	}
+	element.innerText = data;
+}
 
+// Time configs
 function bootstrap() {
-	console.log("Bootsrap home!");
-	const date = DateTime.now()
-		.setZone("America/La_Paz")
-		.minus({ weeks: 1 })
-		.endOf("day")
-		.toISO();
-	console.log("Date,", date);
+	const now = DateTime.now().setZone("America/La_Paz");
+	const year = now.year;
+	let nextEdition = DateTime.fromFormat(`11/11/${year}`, "dd/MM/yyyy").endOf(
+		"day"
+	);
+	console.log(nextEdition, nextEdition);
+	// Check year
+	if (nextEdition <= now) {
+		nextEdition = nextEdition.plus({ year: 1 });
+	}
+
+	setInterval(() => {
+		const timeDistance = nextEdition.diff(
+			DateTime.now().setZone("America/La_Paz"),
+			["days", "hours", "minutes", "seconds"]
+		);
+		const days = timeDistance.days;
+		const hours = timeDistance.hours;
+		const minutes = timeDistance.minutes;
+		const seconds = timeDistance.seconds.toFixed();
+
+		showtimeElement("days", days.toString());
+		showtimeElement("hours", hours.toString());
+		showtimeElement("minutes", minutes.toString());
+		showtimeElement("seconds", seconds.toString());
+	}, 1000);
 }
 
 bootstrap();
